9d22f5c34e877128e38355e8f2b83a4604e7c835
Anandjoe <110010902+Anandjoe@users.noreply.github.com>
2022-10-10 20:44:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ def partition(array, low, high):
 def quick_sort(array, low, high):
     if low < high:
         pi = partition(array, low, high)
-        quick_sort(array,1)
+        quick_sort(array, low, pi - 1)
         quick_sort(array, pi + 1, high)
     return array
  
